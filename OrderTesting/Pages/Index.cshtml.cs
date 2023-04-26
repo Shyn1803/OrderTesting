9e959e432ab538e8fdd41959bcc9771f060dc3c6
Shyn <anhtranvan610@gmail.com>
2023-04-26 10:17:41 +0700

    Add project files.

---
+++ OrderTesting/Pages/Index.cshtml.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace OrderTesting.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public List<ProductInfo> goodsList = new List<ProductInfo>();
+        
+        [BindProperty(SupportsGet = true)]
+        public string searchValue { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            try
+            {
+                String connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=mystore;Integrated Security=True";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "SELECT * FROM chitiet_nhapxuat";
+
+                    if (searchValue?.Length > 0)
+                    {
+                        sql = sql + " WHERE ma_vt LIKE '%" + searchValue + "%' OR ten_vt LIKE '%" + searchValue + "%'";
+                    }
+
+                    sql = sql + ";";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                ProductInfo productInfo = new ProductInfo();
+                                productInfo.id = reader.GetInt32(0);
+                                productInfo.so_phieu = reader.GetString(1);
+                                productInfo.ngay_lap_phieu = reader.GetDateTime(2).ToString("dd/MM/yyyy");
+                                productInfo.ma_vt = reader.GetString(3);
+                                productInfo.ten_vt = reader.GetString(4);
+                                productInfo.dvt = reader.GetString(5);
+                                productInfo.sl_nhap = "" + reader.GetValue(6);
+                                productInfo.sl_xuat = "" + reader.GetValue(7);
+
+                                goodsList.Add(productInfo);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+
+        }
+    }
+
+    public class ProductInfo
+    {
+        public int id;
+        public string so_phieu;
+        public string ngay_lap_phieu;
+        public string ma_vt;
+        public string ten_vt;
+        public string dvt;
+        public string? sl_nhap;
+        public string? sl_xuat;
+    }
+}
---
+++ OrderTesting/Pages/Product/Create.cshtml.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Primitives;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Reflection.Emit;
+
+namespace OrderTesting.Pages.Product
+{
+    public class CreateModel : PageModel
+    {
+        public ProductInfo productInfo = new ProductInfo();
+        public String soPhieuHelper = "";
+        public String ngayLapPhieuHelper = "";
+        public String maVatTuHelper = "";
+        public String tenVatTuHelper = "";
+        public String donViTinhHelper = "";
+        public String errorMessage = "";
+        public String successMessage = "";
+
+        public void OnGet()
+        {
+        }
+
+        private static Boolean HasValue(String value)
+        {   
+            if (value.Length > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static String ErrorMessageHelperField(String field)
+        {
+            String message = "Trường " + field + " là bắt buộc!";
+
+            return message;
+        }
+
+        public void OnPost()
+        {
+            productInfo.so_phieu = Request.Form["soPhieu"];
+            productInfo.ngay_lap_phieu = Request.Form["ngayLapPhieu"];
+            productInfo.ma_vt = Request.Form["maVatTu"];
+            productInfo.ten_vt = Request.Form["tenVatTu"];
+            productInfo.dvt = Request.Form["donViTinh"];
+            productInfo.sl_nhap = StringValues.IsNullOrEmpty(Request.Form["soLuongNhap"]) ? "" : Request.Form["soLuongNhap"];
+            productInfo.sl_xuat = StringValues.IsNullOrEmpty(Request.Form["soLuongXuat"]) ? "" : Request.Form["soLuongXuat"];
+
+            soPhieuHelper = HasValue(productInfo.so_phieu) ? "" : ErrorMessageHelperField("số phiếu");
+            ngayLapPhieuHelper = HasValue(productInfo.ngay_lap_phieu) ? "" : ErrorMessageHelperField("ngày lập phiếu");
+            maVatTuHelper = HasValue(productInfo.ma_vt) ? "" : ErrorMessageHelperField("mã vật tư");
+            tenVatTuHelper = HasValue(productInfo.ten_vt) ? "" : ErrorMessageHelperField("tên vật tư");
+            donViTinhHelper = HasValue(productInfo.dvt) ? "" : ErrorMessageHelperField("đơn vị tính");
+
+            if (!HasValue(productInfo.so_phieu) || !HasValue(productInfo.ngay_lap_phieu) || !HasValue(productInfo.ma_vt) || !HasValue(productInfo.ten_vt) || !HasValue(productInfo.dvt))
+            {
+                return;
+            }
+
+            // Save the new product into DB
+            try
+            {
+                String connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=mystore;Integrated Security=True";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "INSERT INTO chitiet_nhapxuat ";
+                    String queryColumns = "(so_phieu, ngay_lap_phieu, ma_vt, ten_vt, dvt";
+                    String queryParams = "(@so_phieu, @ngay_lap_phieu, @ma_vt, @ten_vt, @dvt";
+                    if (productInfo.sl_nhap.Length > 0)
+                    {
+                        queryColumns = queryColumns + ", sl_nhap";
+                        queryParams = queryParams + ", @sl_nhap";
+                    }
+
+                    if (productInfo.sl_xuat.Length > 0)
+                    {
+                        queryColumns = queryColumns + ", sl_xuat";
+                        queryParams = queryParams + ", @sl_xuat";
+                    }
+                    queryColumns = queryColumns + ") ";
+                    queryParams = queryParams + ");";
+
+                    sql = sql + queryColumns + "VALUES " + queryParams;
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@so_phieu", productInfo.so_phieu);
+                        command.Parameters.AddWithValue("@ngay_lap_phieu", productInfo.ngay_lap_phieu);
+                        command.Parameters.AddWithValue("@ma_vt", productInfo.ma_vt);
+                        command.Parameters.AddWithValue("@ten_vt", productInfo.ten_vt);
+                        command.Parameters.AddWithValue("@dvt", productInfo.dvt);
+                        if (productInfo.sl_nhap.Length > 0)
+                        {
+                            command.Parameters.AddWithValue("@sl_nhap", productInfo.sl_nhap.Length > 0 ? float.Parse(productInfo.sl_nhap) : null);
+                        }
+                        if (productInfo.sl_xuat.Length > 0)
+                        {
+                            command.Parameters.AddWithValue("@sl_xuat", productInfo.sl_xuat.Length > 0 ? float.Parse(productInfo.sl_xuat) : null);
+                        }
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                errorMessage = ex.Message;
+                return;
+            }
+
+            Response.Redirect("/");
+
+        }
+    }
+}
